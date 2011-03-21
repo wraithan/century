ace16b2700441c65e4702b55497cb242b377bf8c
Chris McDonald <XWraithanX@gmail.com>
2011-03-21 02:02:06 -0700

    fab file updates, nginx conf

--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,19 @@
-from fabric.api import env, run, cd, sudo, local, get
-
+from fabric.api import env, run, cd, sudo, local, get, prefix
+from contextlib import contextmanager
 
 env.hosts = ['wraithan.net', ]
-deploy_dir = '/srv/wsgi/century/'
 project_name = 'century'
+deploy_dir = '/srv/wsgi/%s/' % project_name
+activate = 'source /home/wraithan/.virtualenvs/%s/bin/activate' % project_name
 
-def virtualenv_run(cmd):
-    run('workon %s && %s' % (project_name, cmd))
-
+@contextmanager
+def virtualenv():
+    with cd(deploy_dir):
+        with prefix(activate):
+            yield
 
 def virtualenv_local(cmd):
-    local('workon %s && %s' %s (project_name, cmd))
+    local('source /home/wraithan/.virtualenvs/%s/bin/activate && %s' %s (project_name, cmd))
 
 
 def deploy():
@@ -20,26 +23,9 @@ def deploy():
     reload_code()
 
 
-def install():
-    make_deploy_dir()
-    git_clone()
-    make_virtualenv()
-    install_requirements()
-    start_gunicorn()
-    install_nginx_conf()
-    enable_nginx_conf()
-    reload_nginx_conf()
-    create_db()
-    sync_db()
-
 def full_restart_gunicorn():
     stop_gunicorn()
     start_gunicorn()
-
-
-def make_deploy_dir():
-    sudo('mkdir ' + deploy_dir)
-    sudo('chown wraithan:users ' + deploy_dir)
 
 
 def git_clone():
@@ -52,32 +38,19 @@ def git_pull():
         run('git pull')
 
 
-def make_virtualenv():
-    run('mkvirtualenv --no-site-packages %s' % project_name)
-
-
 def install_requirements():
-    with cd(deploy_dir):
-        virtualenv_run('pip install -r --upgrade deploy-requirements.txt')
+    with virtualenv():
+        run('pip install --upgrade -r deploy-requirements.txt')
 
 
 def start_gunicorn():
-    with cd(deploy_dir):
-        virtualenv_run('gunicorn_django --pid=' + deploy_dir +
-                       '/gunicorn.pid --workers=2 -b 127.0.0.1:8020 --daemon deploy_settings.py')
+    with virtualenv():
+        run('gunicorn_django --pid=' + deploy_dir +
+            '/gunicorn.pid --workers=2 -b 127.0.0.1:8020 --daemon deploy_settings.py')
 
 def stop_gunicorn():
     with cd(deploy_dir):
         sudo('kill `cat gunicorn.pid`')
-
-
-def install_nginx_conf():
-    sudo('cp ' + deploy_dir +
-         '/conf/%s /etc/nginx/sites-available/%s' % (project_name, project_name))
-
-
-def enable_nginx_conf():
-    sudo('ln -s /etc/nginx/sites-available/%s /etc/nginx/sites-enabled/%s' % (project_name, project_name))
 
 
 def reload_nginx_conf():
@@ -85,13 +58,9 @@ def reload_nginx_conf():
     sudo('/etc/rc.d/nginx reload')
 
 
-def create_db():
-    run('createdb %s')
-
-
 def sync_db():
-    with cd(deploy_dir):
-        virtualenv_run('./manage.py syncdb')
+    with virtualenv():
+        run('./manage.py syncdb')
 
 
 def reload_code():
