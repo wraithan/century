1ea5b13661c6b84aa2b7676cc5c90b04d25e1ce9
Chris McDonald <XWraithanX@gmail.com>
2011-03-26 12:13:52 -0700

    added the route model

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,13 @@
+from decimal import Decimal
 from django.db import models
+from lib.century.models import TimedeltaField
 
-# Create your models here.
+class Route(models.Model):
+    start = models.CharField(max_length=255)
+    end = models.CharField(max_length=255)
+    distance = models.DecimalField(max_digits=4, decimal_places=2)
+    time = TimedeltaField()
+
+    @property
+    def miles_per_hour(self):
+        return Decimal(self.distance) / Decimal(str(self.time.total_seconds() / 60 / 60))
