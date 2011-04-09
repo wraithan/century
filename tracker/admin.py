c8789773755823a430e3b2c4cc510e7710a97a10
Chris McDonald <XWraithanX@gmail.com>
2011-04-08 23:25:16 -0700

    thing, pigeonhole, etc etc added

---
+++ tracker/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from models import Thing, PigeonHole, Attribute, IntAttribute, TextAttribute
+
+admin.site.register(Thing)
+admin.site.register(PigeonHole)
+admin.site.register(Attribute)
+admin.site.register(IntAttribute)
+admin.site.register(TextAttribute)
+
+
--- tracker/models.py
+++ tracker/models.py
@@ -1,13 +1,40 @@
-from decimal import Decimal
 from django.db import models
-from lib.century.models import TimedeltaField
 
-class Route(models.Model):
-    start = models.CharField(max_length=255)
-    end = models.CharField(max_length=255)
-    distance = models.DecimalField(max_digits=4, decimal_places=2)
-    time = TimedeltaField()
+
+class Thing(models.Model):
+    name = models.CharField(max_length=255)
+    pigeonhole = models.ForeignKey('tracker.PigeonHole')
+
+    def str_all_attributes(self):
+        string = ""
+        for attr in self.attributes.all():
+            string += "%s: %s\n" % (attr.name, attr.subobject.value)
+        return string
+
+class PigeonHole(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+class Attribute(models.Model):
+    name = models.CharField(max_length=255)
+    things = models.ManyToManyField('tracker.Thing', related_name='attributes')
 
     @property
-    def miles_per_hour(self):
-        return Decimal(self.distance) / Decimal(str(self.time.total_seconds() / 60 / 60))
+    def subobject(self):
+        if self.is_int:
+            return self.intattribute
+        elif self.is_text:
+            return self.textattribute
+
+    @property
+    def is_int(self):
+        return IntAttribute.objects.filter(attribute_ptr=self).count() > 0
+
+    @property
+    def is_text(self):
+        return TextAttribute.objects.filter(attribute_ptr=self).count() > 0
+
+class IntAttribute(Attribute):
+    value = models.IntegerField()
+
+class TextAttribute(Attribute):
+    value = models.TextField()
